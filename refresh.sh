06b0b919a5a704a3efd0d9bdc53b37ff75d8aa68
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2020-03-17 17:46:52 +0300

    get rid of ack in favor of grep

--- refresh.sh
+++ refresh.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-cp -fr ~/.config/nvim/colors nvim
 cp -f ~/.config/nvim/init.vim nvim
 
 cp -f ~/.ssh/id_rsa.pub ssh
 cp -f ~/.ssh/config ssh
 
-cp -f ~/.ackrc .
 cp -f ~/.zshrc .
 cp -f ~/.gitconfig .
 
